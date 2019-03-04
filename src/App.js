4be62a58434f54bf63fe95dcb01c4c45d02d2141
Angel Camacho <angelcamacho1708@gmail.com>
2019-03-04 11:55:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/CreateTodo";
+import EditTodo from "./components/EditTodo";
+import TodosList from "./components/TodosList";
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+
+//import logo from "./logo.svg";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <div className="container">
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/profile" component={Profile}/>  
+            <Route exact path="/todo" component={TodosList} />
+            <Route exact path="/edit/:id" component={EditTodo} />
+            <Route exact path="/create" component={CreateTodo} />
+          </div>
+        </div>
+
+      </Router>
     );
   }
 }
